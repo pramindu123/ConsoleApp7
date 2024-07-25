d245ef1f103c8e11aa88b79798a4ded1aa05399f
pramindu123 <pramindukumaranayake999@gmail.com>
2024-07-25 16:21:25 +0530

    commit

---
+++ ConsoleApp7/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double BasicSalary { get; set; }
+
+        public Employee(string afirstName, string aLastName, double aBasicSalary)
+        {
+            FirstName = afirstName;
+            LastName = aLastName;
+            BasicSalary = aBasicSalary;
+        }
+        public void print(string afirstName, string aLastName, double aBasicSalary)
+        {
+            Console.WriteLine($"Name: {afirstName} {aLastName} , Salary :{aBasicSalary}");
+        }
+
+
+    }
+}
--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace ConsoleApp7
+﻿using ConsoleApp7;
+
+namespace ConsoleApp7
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!"sssd);
+            Employee employee1 = new Employee("Chamil", "Jeewantha", 25000.00);
+
+            Employee employee2 = new Employee("pramindu", "vishwa", 28000.00);
+
+            employee1.print(employee1.FirstName, employee1.LastName, employee1.BasicSalary);
+
+
         }
     }
 }
